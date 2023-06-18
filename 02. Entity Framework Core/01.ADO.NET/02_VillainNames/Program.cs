f6e879f7d165ba305be63c747101c681263bec31
Vladimir Savov <93003894+VladimirSavov@users.noreply.github.com>
2023-06-18 15:10:34 +0300

    Add files via upload

---
+++ 02. Entity Framework Core/01.ADO.NET/02_VillainNames/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Net.Security;
+
+namespace _02_VillainNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection conection = new SqlConnection("Server=.;Database=MinionsDB;Integrated Security=true");
+            conection.Open();
+            string query = "SELECT CONCAT(v.Name, ' - ', COUNT(mv.VillainId)) AS Output FROM Villains AS v JOIN MinionsVillains AS mv ON v.Id = mv.VillainId GROUP BY v.Name HAVING COUNT(mv.VillainId) > 3 ORDER BY COUNT(mv.VillainId)";
+            SqlCommand cmd = new SqlCommand(query, conection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string output = (string)reader["Output"];
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ 02. Entity Framework Core/01.ADO.NET/03_MinionNames/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Data.SqlClient;
+using System.Text;
+
+namespace _03_MinionNames
+{
+    public class Program
+    {
+        public const string ConnectionString = @"Server=.;Database=MinionsDB;Integrated Security=true";
+        static void Main(string[] args)
+        {
+            using SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+
+            int villianId = int.Parse(Console.ReadLine());
+
+            string result = GetMinionsInfoAboutVillian(conn, villianId);
+
+            Console.WriteLine(result);
+        }
+
+        private static string GetMinionsInfoAboutVillian(SqlConnection conn, int villianId)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string villianName = GetVillianName(conn, villianId);
+
+            if (villianName == null)
+            {
+                sb.Append($"No villain with ID {villianId} exists int the database.");
+            }
+            else
+            {
+                sb.AppendLine($"Villian: {villianName}");
+                string getMinionsInfoQuerryText = @"SELECT m.Name,m.Age
+	                                                FROM Villains AS v
+	                                                LEFT JOIN MinionsVillains AS mv ON mv.VillainId = v.Id
+	                                                LEFT JOIN Minions AS m ON mv.MinionId = m.Id
+	                                                WHERE v.Name = @villianName
+	                                                ORDER BY m.Name";
+
+                SqlCommand getMinionsinfoCommand = new SqlCommand(getMinionsInfoQuerryText, conn);
+                getMinionsinfoCommand.Parameters.AddWithValue("@villianName", villianName);
+
+                using SqlDataReader reader = getMinionsinfoCommand.ExecuteReader();
+
+                int rowNum = 1;
+                while (reader.Read())
+                {
+                    string minionName = reader["Name"]?.ToString();
+                    string minionAge = reader["Age"]?.ToString();
+
+                    if (minionName == "" && minionAge == "")
+                    {
+                        sb.AppendLine("(no minions)");
+                        break;
+                    }
+
+                    sb.AppendLine($"{rowNum}. {minionName} {minionAge}");
+                    rowNum++;
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string GetVillianName(SqlConnection sqlConnection, int villianId)
+        {
+
+            string getVillianNameQuerryText = @"SELECT Name FROM Villains WHERE Id = @villianId";
+
+            using SqlCommand getVillianNameCommand = new SqlCommand(getVillianNameQuerryText, sqlConnection);
+            getVillianNameCommand.Parameters.AddWithValue("@villianId", villianId);
+
+            string villianName = getVillianNameCommand
+                .ExecuteScalar()?
+                .ToString();
+
+            return villianName;
+        }
+    }
+}
